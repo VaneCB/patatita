b8590b75b04df7be3cd5f786af894111011df891
VaneCB <vcarrerabcn@gmail.com>
2022-02-06 19:54:48 +0100

    Primer commit

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"patatita","l":"Main"}];updateSearchResults();
---
+++ src/patatita/Main.java
@@ -0,0 +1,14 @@
+package patatita;
+
+public class Main {
+
+	/**
+	 * DKJFISAJFLALF j AJFALJAKLFJ A
+	 * @param args ESTASODF AS
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
